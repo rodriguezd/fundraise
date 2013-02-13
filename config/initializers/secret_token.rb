82536cf18299aafb4a076224d792e9c3fa89fde5
David Rodriguez <davidrodriguez212@gmail.com>
2013-02-13 18:14:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fundraise::Application.config.secret_token = '075d22c60cfe56692a7b9d0c991930acfce7d230ab3fa9d02334bc8b4b49556fc94283a40df635e0a5e21ed187237ef7363851f7777a9b435195173511a32cc5'
