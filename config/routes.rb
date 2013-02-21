df4373230e53acddfda3b7de67a328b84f92ac4d
David Rodriguez <davidrodriguez212@gmail.com>
2013-02-21 15:29:33 -0500

    Add static_pages controller, redirect root to 'home'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Fundraise::Application.routes.draw do
+  get "static_pages/home", :as => "home"
+
+  get "static_pages/about", :as => "about"
+
+  get "static_pages/contact", :as => "contact"
+
   # get "sessions/new"
 
   get "events/search" => "events#search", :as => "events_search"
@@ -63,7 +69,7 @@ Fundraise::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#new'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
