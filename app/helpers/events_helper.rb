45ee009a28b506d940fe6e2019ce9885d6591e03
David Rodriguez <davidrodriguez212@gmail.com>
2013-03-10 21:15:08 -0400

    Add event type drop down on event and search forms

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+
+	def event_types
+		[
+			['Bake Sale', 'Bake Sale'],
+			['Stoop Sale', 'Stoop Sale'],
+			['Car Wash', 'Car Wash'],
+			['Garage Sale', 'Garage Sale'],
+			['Carnival', 'Carnival'],
+			['Other', 'Other']
+		]
+	end
+
 end
